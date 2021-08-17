636b45ac83f517cf7df4fc8fc38fcabf9d28dc4f
Anže Pečar <anze@pecar.me>
2021-08-17 01:27:15 +0100

    Bump version from 2021.8.4b0 to 2021.8.17b0 🚀

--- selenium_testing_library/__init__.py
+++ selenium_testing_library/__init__.py
@@ -1,3 +1,3 @@
 from .screen import *
 
-__version__ = "2021.8.4b0"
+__version__ = "2021.8.17b0"
--- tests/test_selenium_testing_library.py
+++ tests/test_selenium_testing_library.py
@@ -14,7 +14,7 @@ from selenium_testing_library import (
 
 
 def test_version():
-    assert __version__ == "2021.8.4b0"
+    assert __version__ == "2021.8.17b0"
 
 
 @pytest.fixture()
